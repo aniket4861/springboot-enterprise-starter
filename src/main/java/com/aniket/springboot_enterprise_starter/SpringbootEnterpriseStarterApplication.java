ad59ad42c2fea66434509e3018e8e4d6dca8d6fb
aniket4861 <aniketpotter16@gmail.com>
2025-07-28 10:59:02 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/aniket/springboot_enterprise_starter/SpringbootEnterpriseStarterApplication.java
@@ -0,0 +1,13 @@
+package com.aniket.springboot_enterprise_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEnterpriseStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEnterpriseStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aniket/springboot_enterprise_starter/SpringbootEnterpriseStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniket.springboot_enterprise_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEnterpriseStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
