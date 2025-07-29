3c002614c962efcace56d639e31ba31acdfe0a45
aniket4861 <aniketpotter16@gmail.com>
2025-07-29 07:39:55 +0530

    swagger added

--- src/main/java/com/aniket/springboot_enterprise_starter/SpringbootEnterpriseStarterApplication.java
+++ src/main/java/com/aniket/springboot_enterprise_starter/SpringbootEnterpriseStarterApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootEnterpriseStarterApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringbootEnterpriseStarterApplication.class, args);
 	}
 
